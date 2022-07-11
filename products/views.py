cec65a8cf249e47936516baf6a2eeb5d15c9fd87
Spiros <geokh@hotmail.com>
2022-07-11 19:06:54 +0300

    Initial commit

---
+++ products/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def index(request):
+    return HttpResponse('Hello World')
+
+
+def new_product(request):
+    return HttpResponse('New Products')
+
