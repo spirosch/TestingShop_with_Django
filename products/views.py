82bee77c9a57973289323d174bcadd5faf59ba53
Spiros <geokh@hotmail.com>
2022-07-15 10:56:03 +0300

    Final touches with Bootstrap changes to settings.py

--- products/views.py
+++ products/views.py
@@ -1,13 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse('Hello World')
+    products = Product.objects.all()
+    return render(request, 'index.html',
+                  {'products': products})
 
 
 def new_product(request):
-    return HttpResponse('New Products')
+    return render(request, 'index.html')
 
